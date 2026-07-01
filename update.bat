8f4db22e748b274ab849998e7374d03e52739b1e
QiuMo246 <13912037680@163.com>
2026-07-01 16:41:09 +0800

    Auto Update 2026/07/01 周三 16:41:09.48

--- update.bat
+++ update.bat
@@ -1,9 +1,11 @@
 @echo off
-
+echo.
+echo ============================
+echo Updating GitHub...
+echo ============================
 git add .
-
-git commit -m "Auto Update"
-
+git commit -m "Auto Update %date% %time%"
 git push
-
+echo.
+echo Done!
 pause
